d5c32e7bd045635e77c4738e344fd02618a10e00
andreee77 <andrea.guaragna@unikey.it>
2023-01-24 15:07:30 +0100

    prima inizializzazione del progetto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './assets/toyota-logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
