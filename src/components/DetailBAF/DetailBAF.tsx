7f5e92b8f700ee23e0ae21b705b6198421856db2
Valerio Caporali <91410329+valeriocaporalix@users.noreply.github.com>
2023-01-24 18:05:31 +0100

    Implementazione routing parametrico (#3)
    * Implementazione routing parametrico
    
    * Aggiunta id parametrico su DetailBAF.tsx

--- src/components/DetailBAF/DetailBAF.tsx
+++ src/components/DetailBAF/DetailBAF.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
 import './DetailBAF.css';
+import {useParams} from "react-router-dom";
 
 interface DetailBafProps {}
 
-const DetailBaf: FC<DetailBafProps> = () => (
-  <div className="DetailBAF">
-    DetailBaf Component
-  </div>
-);
+const DetailBaf: FC<DetailBafProps> = () => {
+    let {id} = useParams();
+
+    return(
+        <div className="DetailBAF">
+            {id}
+        </div>
+    );
+};
 
 export default DetailBaf;
