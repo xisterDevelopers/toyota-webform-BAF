ae15178162912ac2b2875ad93c906299e1eda381
Valerio Caporali <cyndoriil@gmail.com>
2023-01-24 16:14:59 +0100

    Implementazione header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './assets/toyota-logo.svg';
+import logo from './assets/svg/toyota-logo.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App px-5 py-4">
       <header className="App-header">
-
+        <img className="mb-5" src={logo} alt="logo" />
+          <h1 className="font-title black">Welcome on <span className="dark-red">Bank Account Form</span></h1>
       </header>
     </div>
   );
