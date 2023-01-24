88cc5e1e323e3624028c466203fdcf3599395a9d
Valerio Caporali <cyndoriil@gmail.com>
2023-01-24 18:01:33 +0100

    Implementazione routing parametrico

--- src/components/UpsertBAF/UpsertBAF.tsx
+++ src/components/UpsertBAF/UpsertBAF.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import './UpsertBAF.css';
+import {useParams} from "react-router-dom";
 
 interface UpsertBafProps {}
 
-const UpsertBaf: FC<UpsertBafProps> = () => (
-  <div className="UpsertBAF">
-    UpsertBaf Component
-  </div>
-);
+const UpsertBaf: FC<UpsertBafProps> = () => {
+    let {id}= useParams();
+
+
+    return(
+        <div className="UpsertBAF">
+            <p>{id}</p>
+        </div>
+    );
+}
+
 
 export default UpsertBaf;
