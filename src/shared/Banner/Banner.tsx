78a7a0da882008f79daff24c4715e72b1fdb77bc
Valerio Caporali <cyndoriil@gmail.com>
2023-01-25 12:06:20 +0100

    creazione componente Banner

---
+++ src/shared/Banner/Banner.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import './Banner.css';
+
+interface BannerProps {
+    stroke: string;
+    fill: string;
+    content: string;
+    icon: string;
+}
+
+const Banner: FC<BannerProps> = ({stroke,fill,content}) => {
+    return(
+      <div>
+
+      </div>
+    );
+};
+
+export default Banner;
