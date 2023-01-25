f108c1194ba44e7aee64b9dbda30482713faec5e
Valerio Caporali <91410329+valeriocaporalix@users.noreply.github.com>
2023-01-25 15:16:02 +0100

    T 6 (#6)
    * creazione componente Banner
    
    * Implementazione componente Banner.tsx

---
+++ src/shared/Banner/Banner.tsx
@@ -0,0 +1,23 @@
+import React, {FC, ReactElement} from 'react';
+import './Banner.css';
+import Icon from "../Icon/Icon";
+
+interface BannerProps {
+    stroke: string;
+    fill: string;
+    content: ReactElement;
+    icon: string;
+}
+
+const Banner: FC<BannerProps> = ({stroke,fill,content,icon}) => {
+    return(
+      <div className={"banner-container p-4 " + fill + " " + stroke}>
+          <Icon icon={icon} />
+          <div className="mx-3 content-container">
+              {content}
+          </div>
+      </div>
+    );
+};
+
+export default Banner;
