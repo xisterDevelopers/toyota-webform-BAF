a4c2018e00edc923989988ca43b580c933eaafa4
Valerio Caporali <cyndoriil@gmail.com>
2023-01-25 09:23:47 +0100

    react-cli nuovo type

---
+++ src/shared/Icon/Icon.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Icon.css';
+
+interface IconProps {}
+
+const Icon: FC<IconProps> = () => (
+  <div className="Icon">
+    Icon Component
+  </div>
+);
+
+export default Icon;
